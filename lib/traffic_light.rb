369be28b23d408600df2927e09c0b2fe81f643c6
Nicolas Ledez <github@ledez.net>
2012-07-28 22:21:59 +0200

    First commit

---
+++ lib/traffic_light.rb
@@ -0,0 +1,60 @@
+require "serialport"
+
+class TrafficLight
+  attr_reader :message
+
+  def initialize(port="/dev/tty.usbmodemfd121")
+    @port_str = port
+    @baud_rate = 9600
+    @data_bits = 8
+    @stop_bits = 1
+    @parity = SerialPort::NONE
+
+    @sp = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity)
+    @sp.read_timeout = 1000
+
+    clear
+  end
+
+  def read
+    @message = @sp.read.chomp
+  end
+
+  def setColors(colors)
+    read
+    @sp.write colors
+    read
+  end
+
+  def clear
+    setColors(0)
+  end
+
+  def green
+    setColors(1)
+  end
+
+  def orange
+    setColors(2)
+  end
+
+  def green_orange
+    setColors(3)
+  end
+
+  def red
+    setColors(4)
+  end
+
+  def green_red
+    setColors(5)
+  end
+
+  def orange_red
+    setColors(6)
+  end
+
+  def all
+    setColors(7)
+  end
+end
---
+++ spec/traffic_light_spec.rb
@@ -0,0 +1,47 @@
+require "traffic_light"
+
+describe "Can change light colors" do
+  before(:all) do
+    @traffic = TrafficLight.new
+  end
+
+  it "Set off" do
+    @traffic.clear
+    @traffic.message.should == "0 -> ---"
+  end
+
+  it "Set green" do
+    @traffic.green
+    @traffic.message.should == "1 -> --G"
+  end
+
+  it "Set orange" do
+    @traffic.orange
+    @traffic.message.should == "2 -> -O-"
+  end
+
+  it "Set red" do
+    @traffic.red
+    @traffic.message.should == "4 -> R--"
+  end
+
+  it "Set green & orange" do
+    @traffic.green_orange
+    @traffic.message.should == "3 -> -OG"
+  end
+
+  it "Set green & red" do
+    @traffic.green_red
+    @traffic.message.should == "5 -> R-G"
+  end
+
+  it "Set orange & red" do
+    @traffic.orange_red
+    @traffic.message.should == "6 -> RO-"
+  end
+
+  it "Set all light" do
+    @traffic.all
+    @traffic.message.should == "7 -> ROG"
+  end
+end
